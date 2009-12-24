854cabe601d217170c5ca7390aeb014ae92b3cf8
Heinz N. 'Licenser' Gies <heinz@licenser.net>
2009-12-24 17:31:31 +0100

    Added development dependency to lein

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject org.clojars.heinz/Clossher "0.0.1" 
+(defproject org.clojars.heinz/Clossher "0.0.2" 
   :description "Clojure SSH Library wrapper for jsch." 
   :dependencies [
     [org.clojure/clojure "1.1.0-alpha-SNAPSHOT"] 
     [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
-    [com.jcraft/jsch "0.1.42"]])
+    [com.jcraft/jsch "0.1.42"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]])
